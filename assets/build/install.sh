744506dd706726069445926501af23051ff3788a
何军 <hejun@socialpark.com.cn>
2016-04-18 16:44:48 +0800

    更新 gitlab_shell 拉取方式修正

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -46,9 +46,10 @@ exec_as_git git config --global core.autocrlf input
 
 # install gitlab-shell
 echo "Cloning gitlab-shell v.${GITLAB_SHELL_VERSION}..."
-exec_as_git git clone -q -b v${GITLAB_SHELL_VERSION} --depth 1 ${GITLAB_SHELL_CLONE_URL} ${GITLAB_SHELL_INSTALL_DIR}
+exec_as_git git clone ${GITLAB_SHELL_CLONE_URL} ${GITLAB_SHELL_INSTALL_DIR}
 
 cd ${GITLAB_SHELL_INSTALL_DIR}
+exec_as_git git checkout  v${GITLAB_SHELL_VERSION}
 exec_as_git cp -a ${GITLAB_SHELL_INSTALL_DIR}/config.yml.example ${GITLAB_SHELL_INSTALL_DIR}/config.yml
 exec_as_git ./bin/install
 
